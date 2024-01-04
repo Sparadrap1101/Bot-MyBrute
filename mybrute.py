6af8151cb5b7b749c4045ddc4cfc6fa478bb3c27
Sparadrap1101 <alexis111@orange.fr>
2024-01-04 23:33:47 +0100

    Add '.env' informations

--- mybrute.py
+++ mybrute.py
@@ -5,3 +5,9 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+
+load_dotenv()
+PASSWORD = os.getenv('PASSWORD')
+ACCOUNTS = os.getenv('ACCOUNTS')
+
+accountsArray = ACCOUNTS.split(", ")
