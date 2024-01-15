dbfbb4c6d5d8b515d769c59e6ab9d95ecc3a9aa6
Sparadrap1101 <alexis111@orange.fr>
2024-01-15 18:09:45 +0100

    Add script execution time

--- getOpponents.py
+++ getOpponents.py
@@ -3,3 +3,8 @@ import time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+startTime = time.time()
+
+executionTime = time.time() - startTime
+print("\n--- The execution has last for {} seconds. ---".format(round(executionTime, 2)))
