0a72cb92c9fe5f687c7cfa02e1a94f79e05e25c6
Sparadrap1101 <alexis111@orange.fr>
2024-01-24 14:52:59 +0100

    Add tournament part in the 'ProceedAccounts()' function

--- mybrute.py
+++ mybrute.py
@@ -41,3 +41,48 @@ def ProceedAccounts(startAccount):
             time.sleep(3)
 
             hasFightsLeft = True
+
+            try:
+                driver.find_element(By.CLASS_NAME, "css-v3tyeg")
+                hasFightsLeft = False
+
+                print("{} WINS A TOURNAMENT! HE CAN RANK UP!".format(bruteNames[j + 1]))
+            except:
+                try:
+                    nextTournament = driver.find_element(By.CLASS_NAME, "css-1rb3pee")
+                    if not nextTournament.text == "Brute inscrite.":
+                        findTournament = driver.find_element(By.CLASS_NAME, "css-1l4w6pd")
+                        tournament = findTournament.find_element(By.TAG_NAME, "a")
+                        tournament.click()
+                        
+                        time.sleep(2)
+
+                        try:
+                            tournamentSeen = driver.find_element(By.CLASS_NAME, "css-9w9xg7")
+                            tournamentSeen.click()
+
+                            time.sleep(2)
+                        except:
+                            print("TOURNAMENT ALREADY SEEN")
+
+                        driver.get("https://brute.eternaltwin.org/{}/cell".format(bruteNames[j + 1]))
+
+                        time.sleep(2)
+
+                        try:
+                            driver.find_element(By.CLASS_NAME, "css-v3tyeg")
+                            hasFightsLeft = False
+
+                            print("{} WINS A TOURNAMENT! HE CAN RANK UP!".format(bruteNames[j + 1]))
+                        except:
+                            tournamentRegistration = driver.find_element(By.CLASS_NAME, "css-yb6hwx")
+                            tournamentRegistration.click()
+
+                            time.sleep(1)
+                    else: 
+                        print("Already registered in the tournament.")
+                except:
+                    print("TOURNAMENT REGISTRATION FAILED!")
+
+            fightCounter = 0
+            fightsWonBefore = int(driver.find_elements(By.CLASS_NAME, "css-a0dt3d")[1].text)
