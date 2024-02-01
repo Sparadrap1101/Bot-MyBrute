14c7f5e62f0abe5705d7ead7c69f83a0b1a1a9af
Sparadrap1101 <alexis111@orange.fr>
2024-02-01 18:25:34 +0100

    Load password on 'getOpponents.py'

--- getOpponents.py
+++ getOpponents.py
@@ -6,5 +6,8 @@ from selenium.webdriver.common.by import By
 
 startTime = time.time()
 
+load_dotenv()
+PASSWORD = os.getenv('PASSWORD')
+
 executionTime = time.time() - startTime
 print("\n--- The execution has last for {} seconds. ---".format(round(executionTime, 2)))
