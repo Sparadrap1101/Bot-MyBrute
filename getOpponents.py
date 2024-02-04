08fedcf712851173bb85edac8742ec323611a7f0
Sparadrap1101 <alexis111@orange.fr>
2024-02-04 19:13:37 +0100

    Open, quit & resize Chrome window in 'getOpponents.py'

--- getOpponents.py
+++ getOpponents.py
@@ -9,5 +9,11 @@ startTime = time.time()
 load_dotenv()
 PASSWORD = os.getenv('PASSWORD')
 
+driver = webdriver.Chrome()
+driver.set_window_position(0, 0)
+driver.set_window_size(1000, 850)
+
+driver.quit()
+
 executionTime = time.time() - startTime
 print("\n--- The execution has last for {} seconds. ---".format(round(executionTime, 2)))
