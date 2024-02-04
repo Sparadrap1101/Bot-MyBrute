d7875d142f30cc9514e6bab900acf049971123fa
Sparadrap1101 <alexis111@orange.fr>
2024-02-04 19:11:57 +0100

    Add 'checkVersion.py' script

--- checkVersion.py
+++ checkVersion.py
@@ -1,1 +1,13 @@
 from selenium import webdriver
+
+# Créez une instance du webdriver avec un chemin spécifique vers le chromedriver
+# Assurez-vous de remplacer le chemin par le chemin où se trouve votre chromedriver
+driver = webdriver.Chrome()
+
+# Obtenez la version du ChromeDriver à partir de la propriété 'capabilites'
+version_chromedriver = driver.capabilities['chrome']['chromedriverVersion']
+
+print(f"Version de ChromeDriver : {version_chromedriver}")
+
+# Fermez le navigateur
+driver.quit()
