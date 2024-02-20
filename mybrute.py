48261ddf932110aced12dfea4921f5f3c2dde047
Sparadrap1101 <alexis111@orange.fr>
2024-02-20 12:24:54 +0100

    Add class color in comments

--- mybrute.py
+++ mybrute.py
@@ -265,3 +265,17 @@ if __name__ == "__main__":
 ## Donner le nbre de win sur les tournois et contre quel lvl j'ai perdu ?
 ## Gérer les combats contre le boss de clan sur certains compte (autre script ? Qqc de spécial dans le 'accounts' ?)
 ## Optimiser les prints avec le multiprocess
+
+# class color:
+#    PURPLE = '\033[95m'
+#    CYAN = '\033[96m'
+#    DARKCYAN = '\033[36m'
+#    BLUE = '\033[94m'
+#    GREEN = '\033[92m'
+#    YELLOW = '\033[93m'
+#    RED = '\033[91m'
+#    BOLD = '\033[1m'
+#    UNDERLINE = '\033[4m'
+#    END = '\033[0m'
+
+# print(color.BOLD + 'Hello, World!' + color.END)
