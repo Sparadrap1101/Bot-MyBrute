792ce722dd1b82d14562dd6496feca490a30f4e8
Sparadrap1101 <alexis111@orange.fr>
2024-02-24 02:32:31 +0100

    Get password in 'level-up.py'

--- level-up.py
+++ level-up.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 def ProceedAccounts(startAccount, nbreOfAccounts, accountsArray, sizeArray):
-    
+    PASSWORD = os.getenv('PASSWORD')
 if __name__ == "__main__":
 
     startTime = time.time()
