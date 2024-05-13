fbcafba2b4728bec40e95bdcc03d6bed86a1e75e
Alexis Cerio <alexis111@orange.fr>
2024-05-14 00:11:37 +0200

    Cancel update

--- mybrute.py
+++ mybrute.py
@@ -110,6 +110,8 @@ def ProceedAccounts(startAccount, nbreOfAccounts, accountsArray, sizeArray):
                             del tournamentRegistration
 
                             time.sleep(7)
+                    else: 
+                        printArray.append("Account {}: {} - Already registered in the tournament.".format(i + startAccount, bruteNames[j + 1]))
                     del nextTournament
                 except:
                     printArray.append(color.RED + "Account {}: {} - TOURNAMENT REGISTRATION FAILED!".format(i + startAccount, bruteNames[j + 1]) + color.END)
