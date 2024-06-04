26d091d4c59eb3c4cebe1056578d55df8d09dfeb
Alexis Cerio <alexis111@orange.fr>
2024-06-04 23:34:40 +0200

    Labrute update - Fix level-up

--- level-up.py
+++ level-up.py
@@ -63,7 +63,8 @@ def ProceedAccounts(startAccount, nbreOfAccounts, accountsArray, sizeArray):
                 del levelUp
                 print("\n{} NEEDS TO LEVEL UP:".format(bruteNames[j + 1]))
 
-                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"css-1wi9ne9")))
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"css-rqbvn6")))
+
                 choices = driver.find_elements(By.CLASS_NAME, "css-12z2g5x")
                 lastChoice = 0
                 try:
